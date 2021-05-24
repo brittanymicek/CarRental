f4beb5d3ef626bf8c1598c2020d6a841198472b4
Brittany <owens.brittany@gmail.com>
2021-05-24 10:14:58 -0400

    inital commits and html

---
+++ scripts/index.js
@@ -0,0 +1,24 @@
+// hook up a click event handler for the button
+
+
+// RULES FOR DETERMINGNIG THE RENTAL CHARGES:
+// basic car rental is $29.99 per day
+// there is a 30% surcharge on the basic carrental for drivers under 25
+// not all taxes have already been incorporated into the fees
+// calculate the base renal charge for the car ($29.99 * 'numberOfDays')
+// display results below the button
+// once you have car rental amount, use if statements to determine the fees for selected option
+// display options cost at the bottom of the page
+// use an if statement to determine which radio button is selected.
+// radio button - if 'yes' is selected for under 25, calculate the surcharge ('carRental' * .25), otherwise set surcharge to 0
+// display tsurcharge cost at the bottom of the page
+// calculate (add) the car tental, options fees, and surcharge amount to come up with the total due
+// display 'totalDue' at the bottom of the page
+
+
+"use strict";
+
+
+window.onload = function() {
+
+}
